fd138bfb16a7849c8dfc86413e76bfa0c848bd3e
Baumdiggity <atomnomicbomb@gmail.com>
2020-02-01 11:57:10 -0600

    Initial Commit
    I dont know what I am doing

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float turnForce = 1.5f;
+    [SerializeField] float forwardThrust = 3f;
+    [SerializeField] float maxSpeed = 7f;
+    Rigidbody2D _body;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _body = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        if (Input.GetButton("Horizontal"))
+        {
+            _body.AddTorque(-Input.GetAxis("Horizontal") * turnForce);
+        }
+
+        if (Input.GetButton("Vertical"))
+        {
+            _body.AddRelativeForce(Vector2.right * forwardThrust * Input.GetAxis("Vertical"));
+            //Vector2 testForce = new Vector2(Vector2.right * forwardThrust * Input.GetAxis("Vertical"));
+        }
+    }
+}
